060ef31f491934e412848d55b67b3d25763b8f84
JP <jp@jp.nl>
2021-08-13 11:25:14 +0200

    Sass laten zien.  BEM en Atomic CSS besproken. Aantal JS-APIs laten zien (web storage en geolocation)

---
+++ demo/scripts/dom.js
@@ -0,0 +1,16 @@
+
+
+// window.addEventListener('DOMContentLoaded', () => {
+// 	console.log('dom is klaar met parsen');
+// 	document.querySelector('#update-me').innerText = 'wauw';
+// });
+
+// window.addEventListener('load', () => {
+// 	console.log('pagina is klaar met laden');
+
+// 	// plaatjes videos iframes script alllllles
+// });
+
+
+
+document.querySelector('#update-me').innerText = 'wauw';
